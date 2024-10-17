89e9a9107888ce58b822c4721f63eede8bc0b229
Diego Lázaro <dlazarodecastro@gmail.com>
2024-10-17 19:24:03 +0200

    read projects & comments ok

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,50 @@
+import { useState , useEffect } from "react"
+import service from "../services/config"
+import { useNavigate , Link } from "react-router-dom"
 
 function HomePage() {
+
+  const [allProjects, setAllProjects] = useState(null)
+    
+
+  useEffect(() => {
+    getData()
+  },[])
+  
+  const getData = async () => {
+    try {
+      const response = await service.get("/projects")
+      console.log(response.data)
+      setAllProjects(response.data)
+    } catch (error) {
+      console.log("error al traer proyectos",error)
+    }
+  }
+
+  if(allProjects === null){
+    return <div>...spinner</div>
+  }
+
   return (
-    <div>HomePage</div>
+    <div style={{display:"flex",flexWrap:"wrap"}}>
+    {allProjects.map((project) => {
+      return(
+        <div>
+          <Link to={`/projects/${project._id}`}>
+        <h1>{project.name}</h1>
+        <ul>
+        {project.tech.map((tag) => {
+          <li>{tag}</li>
+        })}
+        </ul>
+        <h3>{project.user.username}</h3>
+        <img src={project.user.img} alt="" />
+        <p>{project.description}</p>
+        </Link>
+      </div>  
+      )
+    })}
+    </div>
   )
 }
 
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import service from "../services/config";
+import { useState , useEffect , useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from '../context/auth.context'
+import { useParams } from "react-router-dom";
 
 function ProjectPage() {
+
+  const {projectId} = useParams()
+
+  const [projectInfo, setProjectInfo] = useState(null)
+  const [allComments, setAllComments] = useState(null)
+
+  const {loggedUserId} = useContext(AuthContext)
+
+  useEffect(() => {
+    getData()
+  },[])
+
+  const getData = async () => {
+    try {
+      const response = await service.get(`/projects/${projectId}`)
+      setProjectInfo(response.data)
+      console.log(response.data)
+      const commentsList = await service.get(`/comments/${projectId}`)
+      setAllComments(commentsList.data)
+      console.log(commentsList.data)
+    } catch (error) {
+      console.log("error al traer la data de un projecto",error)
+    }
+  }
+  
+  if(projectInfo === null || allComments === null){
+    return <div>...spinner</div>
+  } else {
+    const {name,github,deployment,creationDate,description,screenshots,tech,user,collaborators,likes} = projectInfo
+    console.log(name)
   return (
-    <div>ProjectPage</div>
-  )
+    <div>
+      <h1>{name}</h1>
+      <h2>{user.username}</h2>
+      <p>{description}</p>
+      <h3>Likes: {likes.length}</h3>
+      <h4>Comments:</h4>
+      {allComments.map((comment,index) => {
+        return(
+          <div key={index}>
+          <h5>{comment.user.username}</h5>
+          <p>{comment.content}</p>
+        </div>
+        )
+      })}
+    </div>
+  )}
 }
 
 export default ProjectPage
